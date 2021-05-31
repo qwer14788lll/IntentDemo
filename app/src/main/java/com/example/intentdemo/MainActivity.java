74198d5e972ee76593e7a796dba79ea74af1800a
SP6 <qwer14788lll@163.com>
2021-05-31 14:38:44 +0800

    实现了显示跳转功能演示

--- app/src/main/java/com/example/intentdemo/MainActivity.java
+++ app/src/main/java/com/example/intentdemo/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.intentdemo.databinding.ActivityMainBinding;
 
 /**
  * @author 龚鸿飞
  */
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.ButtonToMain.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,HelloActivity.class);
+            startActivity(intent);
+        });
     }
 }
