03ee2bde3da17d40ff80f7d6bf3297df8d982c77
SP6 <qwer14788lll@163.com>
2021-06-11 11:05:30 +0800

    实现了登录（普通方式）功能

--- app/src/main/java/com/example/intentdemo/HomeActivity.java
+++ app/src/main/java/com/example/intentdemo/HomeActivity.java
@@ -2,16 +2,33 @@ package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.intentdemo.databinding.ActivityHomeBinding;
 
 /**
  * @author Surface Pro 6
  */
 public class HomeActivity extends AppCompatActivity {
 
+    private ActivityHomeBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        mBinding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        //获取传过来的Intent
+        Intent intent = getIntent();
+        //获取传过来的数据
+        String user = "账号：" + intent.getStringExtra("user");
+        String pwd = "密码：" + intent.getStringExtra("pwd");
+        String isAdmin = "是否为管理员：" + intent.getStringExtra("isAdmin");
+        //将数据渲染到界面上
+        mBinding.userText.setText(user);
+        mBinding.pwdText.setText(pwd);
+        mBinding.isAdminText.setText(isAdmin);
     }
 }
